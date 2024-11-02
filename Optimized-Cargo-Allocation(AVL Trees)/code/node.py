6cca99971b28aa63c69ed65e7ae19efa85eba2ac
Ishan Rehal <cs1230019@iitd.ac.in>
2024-11-02 15:26:45 +0530

    d

--- Optimized-Cargo-Allocation(AVL Trees)/code/node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, key, parent=None):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.parent = parent  # Parent pointer
-        self.height = 1  # Height of this node
-        pass
